4572b444877a0bd7ca3c7fcc2863801dc6946138
akmatoff <akmatoff.exe@gmail.com>
2022-04-18 12:48:20 +0600

    change button state depending on input

--- script/script.js
+++ script/script.js
@@ -4,11 +4,17 @@ const results = document.querySelector('#results')
 
 let buttonDisabled = true
 let isLoading = false
+let word = ""
 let slogans = [
     {text: 'Testing slogan'}, 
     {text: 'Imagine like it is a catchy slogan here'},
     {text: 'Catchy slogan'},
 ]
+
+const setButtonState = () => {
+    if (buttonDisabled) generatorButton.style.pointerEvents = 'none'
+    else generatorButton.style.pointerEvents = 'all'
+}
 
 const displaySlogans = () => {
     !isLoading && slogans.map((slogan) => {
@@ -34,4 +40,26 @@ const LoadingSpinner = () => {
     return div
 }
 
+onInputChange = (e) => {
+    word = e.target.value
+
+    if (word.length >= 3) buttonDisabled = false
+    else buttonDisabled = true
+
+    setButtonState();
+
+    !buttonDisabled ? generatorButton.classList.remove('disabled')
+    : generatorButton.classList.add('disabled')
+}
+
+onGeneratorButtonClick = (e) => {
+    if (buttonDisabled) return;
+
+    console.log('should send a request')
+}
+
+generatorInput.addEventListener('input', onInputChange)
+generatorButton.addEventListener('click', onGeneratorButtonClick)
+
 displaySlogans()
+setButtonState()
